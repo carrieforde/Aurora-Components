052c2bdbe3921eb03a43251b3896709ade882f07
Carrie Forde <carrie@carrieforde.com>
2017-10-03 09:34:57 -0700

    Adds remaining aria attributes

--- components/accordion/accordion.js
+++ components/accordion/accordion.js
@@ -4,25 +4,56 @@
 ( function( window ) {
 
 	// Get global variables.
-	var accordion = document.getElementById( 'accordion' ),
-		items     = accordion.querySelectorAll( 'li' );
+	var accordions = document.querySelectorAll('.accordion');
 
 	/**
-	 * Show content of currently selected accordion tab.
+	 * Toggle the accordion content panel.
 	 * 
+	 * @param {any} event 
 	 */
-	var showContent = function() {
+	var togglePanel = function (event) {
 
-		// Get local variables.
-		var currentActive = document.querySelector( '.is-active' );
+		var target = event.target,
+			panel  = target.nextElementSibling,	
+			parent = target.parentElement;
 
-		// Remove & add is-active class accordingly.
-		currentActive.classList.remove( 'is-active' );
-		this.classList.add( 'is-active' );
+		if (parent.classList.contains('is-active')) {
+			deactivatePanel(target, panel);
+		} else {
+			activatePanel(target, panel);
+		}
+	};
+
+	/**
+	 * Deactivates a tab & panel.
+	 * 
+	 * @param {string}  tab    The tab element.
+	 * @param {string}  panel  The panel element.
+	 */
+	var deactivatePanel = function (tab, panel) {
+
+		tab.parentElement.classList.remove('is-active');
+		tab.setAttribute('aria-selected', 'false');
+		panel.removeAttribute('aria-expanded');
+		panel.setAttribute('aria-hidden', 'true');
+	};
+
+	/**
+	 * Activates a tab & panel.
+	 * 
+	 * @param {string}  tab    The tab element.
+	 * @param {string}  panel  The panel element.
+	 */
+	var activatePanel = function (tab, panel) {
+		
+		tab.parentElement.classList.add('is-active');
+		tab.setAttribute('aria-selected', 'true');
+		panel.removeAttribute('aria-hidden');
+		panel.setAttribute('aria-expanded', 'true');
 	};
 
 	// Add event listeners.
-	for ( var i = 0; i < items.length; i++ ) {
-		items[i].addEventListener( 'click', showContent );
+	for (var i = 0; i < accordions.length; i++) {
+		accordions[i].addEventListener('click', togglePanel);
 	}
 })( window );
