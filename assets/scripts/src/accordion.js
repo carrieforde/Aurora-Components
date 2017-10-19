58fac3c34d2984333528f56c020a2a6fc372cf01
Carrie Forde <carrie@carrieforde.com>
2017-10-19 16:50:25 -0700

    Adds basic keyboard navigation

--- assets/scripts/src/accordion.js
+++ assets/scripts/src/accordion.js
@@ -1,59 +1,111 @@
 /**
  * Accordion scripts.
  */
-( function( window ) {
+( function() {
 
 	// Get global variables.
 	var accordions = document.querySelectorAll('.accordion');
 
 	/**
 	 * Toggle the accordion content panel.
-	 * 
-	 * @param {any} event 
+	 *
+	 * @param {any} event
 	 */
-	var togglePanel = function (event) {
+	function togglePanel(event) {
+
+		// Bail if the event target isn't the button.
+		if (!event.target.classList.contains('accordion__button')) {
+			return;
+		}
 
 		var target = event.target,
-			panel  = target.nextElementSibling,	
-			parent = target.parentElement;
+			parent = target.parentElement.parentElement;
 
 		if (parent.classList.contains('is-active')) {
-			deactivatePanel(target, panel);
+			deactivatePanel(target, parent);
 		} else {
-			activatePanel(target, panel);
+			activatePanel(target, parent);
 		}
-	};
+	}
 
 	/**
 	 * Deactivates a tab & panel.
-	 * 
+	 *
 	 * @param {string}  tab    The tab element.
 	 * @param {string}  panel  The panel element.
 	 */
-	var deactivatePanel = function (tab, panel) {
+	function deactivatePanel(tab, parent) {
 
-		tab.parentElement.classList.remove('is-active');
-		tab.setAttribute('aria-selected', 'false');
-		panel.removeAttribute('aria-expanded');
-		panel.setAttribute('aria-hidden', 'true');
-	};
+		tab.setAttribute('aria-expanded', 'false');
+		parent.classList.remove('is-active');
+	}
 
 	/**
 	 * Activates a tab & panel.
-	 * 
+	 *
 	 * @param {string}  tab    The tab element.
 	 * @param {string}  panel  The panel element.
 	 */
-	var activatePanel = function (tab, panel) {
-		
-		tab.parentElement.classList.add('is-active');
-		tab.setAttribute('aria-selected', 'true');
-		panel.removeAttribute('aria-hidden');
-		panel.setAttribute('aria-expanded', 'true');
-	};
+	function activatePanel(tab, parent) {
+
+		tab.setAttribute('aria-expanded', 'true');
+		parent.classList.add('is-active');
+	}
+
+	/**
+	 * Adds navigation through up / down / left / right arrow keys.
+	 *
+	 * @param {any} event
+	 */
+	function keyboardNav(event) {
+
+		var key = event.keyCode,
+			target = event.target,
+			parent = target.parentElement.parentElement,
+			accordion = parent.parentElement,
+			newTarget;
+
+		switch (key) {
+
+			// If up or left key is pressed.
+			case 37:
+			case 38:
+
+				// Set the new target.
+				if (parent.previousElementSibling === null) {
+					newTarget = accordion.querySelectorAll('.accordion__button');
+					newTarget = newTarget[newTarget.length - 1];
+				} else {
+					newTarget = parent.previousElementSibling;
+					newTarget = newTarget.querySelector('.accordion__button');
+				}
+
+				newTarget.focus();
+				break;
+
+			// If down or right key is pressed.
+			case 39:
+			case 40:
+
+				// Set the new target.
+				if (parent.nextElementSibling === null) {
+					newTarget = accordion.querySelector('.accordion__button');
+				} else {
+					newTarget = parent.nextElementSibling;
+					newTarget = newTarget.querySelector('.accordion__button');
+				}
+
+				newTarget.focus();
+				break;
+
+			default:
+				break;
+		}
+	}
 
 	// Add event listeners.
 	for (var i = 0; i < accordions.length; i++) {
 		accordions[i].addEventListener('click', togglePanel);
+		accordions[i].addEventListener('keyup', keyboardNav);
 	}
-})( window );
+})();
