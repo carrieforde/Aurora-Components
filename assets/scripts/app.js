ae4c409ade5fdf1f6102b231f19dd719ab7b75c8
Carrie Forde <carrie@carrieforde.com>
2017-10-19 13:48:58 -0700

    compiled scripts & styles

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,59 +1,6 @@
 /*! bov-web-components JS - This file is built with Grunt. DO NOT EDIT! */
 
 /**
- * Add smooth scrolling for on-page navigation.
- */
-(function () {
-
-	// Script options.
-	var options = {
-		menuSelector: '.nav-menu .menu',
-		mobileBreakPoint: 900,
-		headerHeight: 0,
-		timeout: 500 // in milleseconds
-	},
-	    menu = document.querySelector(options.menuSelector);
-
-	/**
-  * Do smooth scrolling.
-  *
-  * {@link  https://css-tricks.com/snippets/jquery/smooth-scrolling/}
-  * {@link  https://css-tricks.com/smooth-scrolling-accessibility/}
-  * @param  {any}  event 
-  */
-	function smoothScroll(event) {
-
-		event.preventDefault();
-
-		var el = event.target,
-		    hash = el.getAttribute('href'),
-		    url = window.location.pathname,
-		    target = document.querySelector(hash),
-		    offset = target.offsetTop;
-
-		// Check window width, and update offset accordingly.
-		if (window.innerWidth > options.mobileBreakPoint - 1) {
-			offset = offset - options.headerHeight;
-		}
-
-		// Scroll the the desired element.
-		window.scroll({ left: 0, top: offset, behavior: 'smooth' });
-
-		// Update focus after scolling is complete.
-		setTimeout(function () {
-
-			// Updates focus to our target element.
-			target.setAttribute('tabindex', '-1');
-			target.focus();
-			window.location.href = url + hash;
-			window.scroll(0, offset); // prevents jumpbacks from applying focus.
-		}, options.timeout);
-	}
-
-	menu.addEventListener('click', smoothScroll);
-})();
-
-/**
  * Accordion scripts.
  */
 (function (window) {
@@ -241,6 +188,59 @@
 
 	window.addEventListener('load', addClasses);
 	navMenu.addEventListener('keyup', tabNavigation);
+})();
+
+/**
+ * Add smooth scrolling for on-page navigation.
+ */
+(function () {
+
+	// Script options.
+	var options = {
+		menuSelector: '.nav-menu .menu',
+		mobileBreakPoint: 900,
+		headerHeight: 0,
+		timeout: 500 // in milleseconds
+	},
+	    menu = document.querySelector(options.menuSelector);
+
+	/**
+  * Do smooth scrolling.
+  *
+  * {@link  https://css-tricks.com/snippets/jquery/smooth-scrolling/}
+  * {@link  https://css-tricks.com/smooth-scrolling-accessibility/}
+  * @param  {any}  event 
+  */
+	function smoothScroll(event) {
+
+		event.preventDefault();
+
+		var el = event.target,
+		    hash = el.getAttribute('href'),
+		    url = window.location.pathname,
+		    target = document.querySelector(hash),
+		    offset = target.offsetTop;
+
+		// Check window width, and update offset accordingly.
+		if (window.innerWidth > options.mobileBreakPoint - 1) {
+			offset = offset - options.headerHeight;
+		}
+
+		// Scroll the the desired element.
+		window.scroll({ left: 0, top: offset, behavior: 'smooth' });
+
+		// Update focus after scolling is complete.
+		setTimeout(function () {
+
+			// Updates focus to our target element.
+			target.setAttribute('tabindex', '-1');
+			target.focus();
+			window.location.href = url + hash;
+			window.scroll(0, offset); // prevents jumpbacks from applying focus.
+		}, options.timeout);
+	}
+
+	menu.addEventListener('click', smoothScroll);
 })();
 
 /**
