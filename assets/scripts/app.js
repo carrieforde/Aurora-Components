d714f64da44131318968d9e6babcf1cf33b1bd50
Carrie Forde <carrie@carrieforde.com>
2017-10-19 20:42:37 -0700

    compiled scripts & styles

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -115,45 +115,101 @@
 /**
  * Carousel
  */
-(function (window) {
+(function () {
 
-	// Get global variables.
-	var thumbs = document.querySelectorAll('.carousel__nav li');
+	var carousels = document.querySelectorAll('.carousel');
 
-	/**
-  * Change slides when thumbnail is clicked.
-  * 
-  */
-	var changeSlide = function () {
+	function changeSlides(el) {
 
-		var slides = document.querySelectorAll('.carousel__slide'),
-		    activeSlide,
-		    newSlide;
+		var carousel = el.parentElement.parentElement,
+		    currentDot = carousel.querySelector('.carousel__dots .is-active'),
+		    currentSlide = carousel.querySelector('.carousel__slide.is-active'),
+		    target = el.getAttribute('href'),
+		    newSlide = carousel.querySelector(target);
 
-		// Cycle through slides...
-		for (var i = 0; i < slides.length; i++) {
+		// Update classes.
+		currentDot.classList.remove('is-active');
+		currentSlide.classList.remove('is-active');
+		el.classList.add('is-active');
+		newSlide.classList.add('is-active');
+	}
 
-			// Figure out which slide is currently active.
-			if (slides[i].classList.contains('is-active')) {
-				activeSlide = slides[i];
-			}
+	function advanceSlides(el) {
 
-			// Figure our which slide needs to be active.
-			if (slides[i].dataset.slide === this.dataset.thumb) {
-				newSlide = slides[i];
-			}
+		var carousel = el.parentElement.parentElement,
+		    currentDot = carousel.querySelector('.carousel__dots .is-active'),
+		    nextDot = currentDot.nextElementSibling,
+		    currentSlide = carousel.querySelector('.carousel__slide.is-active'),
+		    nextSlide = currentSlide.nextElementSibling;
+
+		// If we're on the last slide, let's go back to the beginning.
+		if (nextDot === null) {
+			nextDot = carousel.querySelector('.carousel__dots a');
+			nextSlide = carousel.querySelector('.carousel__slide');
 		}
 
 		// Update classes.
-		activeSlide.classList.remove('is-active');
-		newSlide.classList.add('is-active');
-	};
-
-	// Add event listeners to thumbails.
-	for (var i = 0; i < thumbs.length; i++) {
-		thumbs[i].addEventListener('click', changeSlide);
+		currentDot.classList.remove('is-active');
+		currentSlide.classList.remove('is-active');
+		nextDot.classList.add('is-active');
+		nextSlide.classList.add('is-active');
 	}
-})(window);
+
+	function reverseSlides(el) {
+		var carousel = el.parentElement.parentElement,
+		    currentDot = carousel.querySelector('.carousel__dots .is-active'),
+		    prevDot = currentDot.previousElementSibling,
+		    currentSlide = carousel.querySelector('.carousel__slide.is-active'),
+		    prevSlide = currentSlide.previousElementSibling;
+
+		// If we're on the first slide, let's go to the end.
+		if (prevDot === null) {
+			prevDot = carousel.querySelectorAll('.carousel__dots a');
+			prevDot = prevDot[prevDot.length - 1];
+			prevSlide = carousel.querySelectorAll('.carousel__slide');
+			prevSlide = prevSlide[prevSlide.length - 1];
+		}
+
+		// Update classes.
+		currentDot.classList.remove('is-active');
+		currentSlide.classList.remove('is-active');
+		prevDot.classList.add('is-active');
+		prevSlide.classList.add('is-active');
+	}
+
+	function fireClickEvents(event) {
+
+		var el = event.target;
+
+		if (el.classList.contains('carousel__button--next') || el.classList.contains('fa-chevron-right')) {
+
+			if (el.classList.contains('fa-chevron-right')) {
+				el = el.parentElement;
+			}
+
+			advanceSlides(el);
+		}
+
+		if (el.classList.contains('carousel__button--previous') || el.classList.contains('fa-chevron-left')) {
+
+			if (el.classList.contains("fa-chevron-left")) {
+				el = el.parentElement;
+			}
+
+			reverseSlides(el);
+		}
+
+		if (el.tagName.toLowerCase() === 'a') {
+			event.preventDefault();
+
+			changeSlides(el);
+		}
+	}
+
+	for (var i = 0, len = carousels.length; i < len; i++) {
+		carousels[i].addEventListener('click', fireClickEvents);
+	}
+})();
 
 /**
  * Menu JS
