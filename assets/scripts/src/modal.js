b4d1767882983058a1fd30ee8ad190ec5fe76b72
Carrie Forde <carrie@carrieforde.com>
2017-11-16 18:07:46 -0800

    Comments files out until they are reworked for Handlebars

--- assets/scripts/src/modal.js
+++ assets/scripts/src/modal.js
@@ -1,81 +1,81 @@
-/**
- * Modal Scripts
- */
-(function() {
+// /**
+//  * Modal Scripts
+//  */
+// (function() {
 
-	var components = document.querySelectorAll('.modal'),
-		container = document.querySelector('.modal-container'),
-		openedModal;
+// 	var components = document.querySelectorAll('.modal'),
+// 		container = document.querySelector('.modal-container'),
+// 		openedModal;
 
-	/**
-	 * Open the modal a modal.
-	 * 
-	 * @param {string}  instance  The modal to open.
-	 */
-	window.openModal = function(instance) {
+// 	/**
+// 	 * Open the modal a modal.
+// 	 * 
+// 	 * @param {string}  instance  The modal to open.
+// 	 */
+// 	window.openModal = function(instance) {
 
-		var modal    = document.querySelector('#' + instance),
-			wrapper  = modal.parentElement,
-			body     = document.querySelector('body');
+// 		var modal    = document.querySelector('#' + instance),
+// 			wrapper  = modal.parentElement,
+// 			body     = document.querySelector('body');
 
-		openedModal = modal;
+// 		openedModal = modal;
 
-		modal.classList.remove('is-hidden');
-		wrapper.classList.remove('is-hidden');
-		body.classList.add('modal-open');
-	};
+// 		modal.classList.remove('is-hidden');
+// 		wrapper.classList.remove('is-hidden');
+// 		body.classList.add('modal-open');
+// 	};
 
-	/**
-	 * Close currently open modal.
-	 * 
-	 * @param {string}  instance  The modal to close.
-	 */
-	function closeModal(instance) {
+// 	/**
+// 	 * Close currently open modal.
+// 	 * 
+// 	 * @param {string}  instance  The modal to close.
+// 	 */
+// 	function closeModal(instance) {
 
-		var body = document.querySelector('body');
+// 		var body = document.querySelector('body');
 
-		body.classList.remove('modal-open');
-		instance.classList.add('is-hidden');
-		instance.parentElement.classList.add('is-hidden');
+// 		body.classList.remove('modal-open');
+// 		instance.classList.add('is-hidden');
+// 		instance.parentElement.classList.add('is-hidden');
 
-		setTimeout(function() {
-			openedModal = '';
-		}, 200);
-	}
+// 		setTimeout(function() {
+// 			openedModal = '';
+// 		}, 200);
+// 	}
 
-	/**
-	 * Handles click events.
-	 * 
-	 * @param {any} event 
-	 */
-	function handleClickEvents(event) {
+// 	/**
+// 	 * Handles click events.
+// 	 * 
+// 	 * @param {any} event 
+// 	 */
+// 	function handleClickEvents(event) {
 
-		var el = event.target;
+// 		var el = event.target;
 
-		if ( el.classList.contains('button--close') || el.classList.contains('modal-container')) {
-			closeModal(openedModal);
-		}
-	}
+// 		if ( el.classList.contains('button--close') || el.classList.contains('modal-container')) {
+// 			closeModal(openedModal);
+// 		}
+// 	}
 
-	/**
-	 * Closes modal when ESC is presssed.
-	 * 
-	 * @param {any} event 
-	 */
-	function keyClose(event) {
+// 	/**
+// 	 * Closes modal when ESC is presssed.
+// 	 * 
+// 	 * @param {any} event 
+// 	 */
+// 	function keyClose(event) {
 
-		var key = event.keyCode;
+// 		var key = event.keyCode;
 
-		if (key === 27 && openedModal) {
-			closeModal(openedModal);
-		}
-	}
+// 		if (key === 27 && openedModal) {
+// 			closeModal(openedModal);
+// 		}
+// 	}
 
-	// Add event listeners to each instance of a modal.
-	for (var i = 0; i < components.length; i++) {
-		components[i].addEventListener('click', handleClickEvents);
-	}
+// 	// Add event listeners to each instance of a modal.
+// 	for (var i = 0; i < components.length; i++) {
+// 		components[i].addEventListener('click', handleClickEvents);
+// 	}
 
-	container.addEventListener('click', handleClickEvents);
-	document.addEventListener('keyup', keyClose);
-})();
+// 	container.addEventListener('click', handleClickEvents);
+// 	document.addEventListener('keyup', keyClose);
+// })();
