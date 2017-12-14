e216a2b750e07d6a745c0b88f69e0fe49f9fd126
Carrie Forde <carrie@carrieforde.com>
2017-12-14 11:45:55 -0800

    Adds position helper

--- assets/scripts/handlebars.js
+++ assets/scripts/handlebars.js
@@ -32,6 +32,59 @@
 		xhr.send();
 	}
 
+	Handlebars.registerHelper('position', function (num, index, options) {
+
+		if (!options.hash.operator) {
+			if (num === index + 1) {
+				return options.fn(this);
+			}
+		}
+
+		if (options.hash.operator === '==') {
+			if (num == index + 1) {
+				return options.fn(this);
+			}
+		}
+
+		if (options.hash.operator === '!=') {
+			if (num != index + 1) {
+				return options.fn(this);
+			}
+		}
+
+		if (options.has.operator === '!==') {
+			if (num !== index + 1) {
+				return options.fn(this);
+			}
+		}
+
+		if (options.hash.operator === '<') {
+			if (num < index + 1) {
+				return options.fn(this);
+			}
+		}
+
+		if (options.hash.operator === '<=') {
+			if (num <= index + 1) {
+				return options.fn(this);
+			}
+		}
+
+		if (options.hash.operator === '>') {
+			if (num > index + 1) {
+				return options.fn(this);
+			}
+		}
+
+		if (options.hash.operator === '>=') {
+			if (num >= index + 1) {
+				return options.fn(this);
+			}
+		}
+
+		return options.inverse(this);
+	});
+
 	Handlebars.registerHelper('componentCode', function() {
 
 		var output = '';
