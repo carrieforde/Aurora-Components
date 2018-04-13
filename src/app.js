d8e416314f96e227f63269f3ebfc9ef631d1c073
Carrie Forde <carrie@carrieforde.com>
2018-04-13 14:32:10 -0700

    Adds site header & navigation

--- src/app.js
+++ src/app.js
@@ -1,5 +1,9 @@
+/* eslint-disable */
 import preload from '../data';
+import siteData from '../site-data';
 import './helpers';
+import welcome from './includes/welcome';
+import header from './includes/header';
 import Accordion from './components/accordion/index';
 import accordion from './components/accordion/accordion';
 import Tabs from './components/tabs/index';
@@ -10,12 +14,40 @@ import './sass/main';
 
 const app = document.getElementById('app');
 
-app.innerHTML = accordion({ preload });
-app.innerHTML += tabs({ preload });
-app.innerHTML += carousel({ preload });
+app.innerHTML = `
+  ${header({ siteData })}
+  <main class="page__content">${welcome()}</main>
+`;
 
-document.addEventListener('DOMContentLoaded', () => {
-  new Accordion();
-  new Tabs();
-  new Carousel();
-});
+window.addEventListener('hashchange', getTemplate);
+
+function getTemplate(event) {
+  const templateName = event.newURL.split('#')[1],
+    pageContent = document.querySelector('.page__content');
+
+  switch (templateName) {
+    case '':
+    case undefined:
+      pageContent.innerHTML = welcome();
+      break;
+
+    case 'accordion':
+      pageContent.innerHTML = accordion({ preload });
+      new Accordion();
+      break;
+
+    case 'carousel':
+      pageContent.innerHTML = carousel({ preload });
+      new Carousel();
+      break;
+
+    case 'tabs':
+      pageContent.innerHTML = tabs({ preload });
+      new Tabs();
+      break;
+
+    default:
+      `<h2>Sorry, ${templateName} can't be found!</h2>`;
+      break;
+  }
+}
--- src/helpers.js
+++ src/helpers.js
@@ -1,8 +1,11 @@
 import Handlebars from 'handlebars-template-loader/runtime';
+import navigation from './includes/navigation';
 import Utilities from './utilities';
 
 const utils = new Utilities();
 
-Handlebars.registerHelper('camelCase', string => {
-  return utils.camelCase(string);
-});
+Handlebars.registerHelper('camelCase', string => utils.camelCase(string));
+
+Handlebars.registerHelper('lowerCase', string => string.toLowerCase());
+
+Handlebars.registerPartial('navigation', navigation);
